0d4f47572ddbb244fb8ab8a420bfe49201d98f70
AndreyShulgin <neonod404@gmail.com>
2020-08-06 15:41:12 +0300

    3. Интерфейс Input [#181092]

---
+++ src/main/java/ru/job4j/ru/job4j/poly/Bus.java
@@ -0,0 +1,22 @@
+package ru.job4j.ru.job4j.poly;
+
+/**
+ * @author Andrey Shulgin (neonod404@gmail.com)
+ */
+public class Bus implements Transport {
+
+    @Override
+    public void drive() {
+
+    }
+
+    @Override
+    public void passengers(int number) {
+
+    }
+
+    @Override
+    public int fill(int fuel) {
+        return 0;
+    }
+}
